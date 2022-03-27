ac6225eab3810fab9251b4d7f101f20a4700dfa3
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-03-26 21:43:15 -0500

    implementadas migraciones para registro de cambios en la bdd

---
+++ db/migrations/20220327022136-create-product-category-order.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
+const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
+const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+  },
+};
